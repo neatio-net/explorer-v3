676f07020f747eafb956c095c3f687e6038fc428
MrTizu <99123684+MrTizu@users.noreply.github.com>
2023-09-19 07:58:50 +0300

    Update transactionInfosController.js

--- scripts/controllers/transactionInfosController.js
+++ scripts/controllers/transactionInfosController.js
@@ -1,4 +1,4 @@
-angular.module('ethExplorer.tx', ['ngRoute','ui.bootstrap'])
+angular.module('nioExplorer.tx', ['ngRoute','ui.bootstrap'])
     .controller('transactionInfosCtrl', function ($rootScope, $scope, $location, $routeParams,$q) {
 
        var web3 = $rootScope.web3;
